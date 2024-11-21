d823cba3d008d190116d06d9f10dd490c2acc97e
aibeknet <aaskarov0690.kg@gmail.com>
2024-11-21 23:43:09 +0600

    Added axiosApi, Redux store, useAppDispatch hook, and Redux store integration in main.tsx.

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
 import { Provider } from 'react-redux';
+import { store } from './app/store.ts';
 
 
 createRoot(document.getElementById("root")!).render(
-  <Provider store={}>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
