968c44143f46a943050282742b49fc71aec6aecc
aibeknet <aaskarov0690.kg@gmail.com>
2024-11-26 22:38:52 +0600

    Added in App component main routers.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
+import { Route, Routes } from 'react-router-dom';
+import AdminPage from './containers/Admin/AdminPage';
+import DishesList from './containers/DishesList/DishesList';
+import EditPizza from './containers/EditPizza/EditPizza';
+import ClientPage from './containers/ClientPage/ClientPage';
+import Order from './containers/Admin/Order/Order';
+import Orders from './containers/Admin/Orders/Orders';
+import NewPizza from './containers/NewPizza/NewPIzza.tsx';
+
 const App = () => {
   return (
-    <div>
-      Work!
-    </div>
+    <Routes>
+      <Route path="/" element={<ClientPage />} />
+      <Route path="order" element={<Order />} />
+      <Route path="/admin" element={<AdminPage />}>
+        <Route path="dishes" element={<DishesList />} />
+        <Route path="newPizza" element={<NewPizza />} />
+        <Route path="editPizza/:pizzaId" element={<EditPizza />} />
+        <Route path="orders" element={<Orders />} />
+      </Route>
+    </Routes>
   );
 };
 
 export default App;
+
